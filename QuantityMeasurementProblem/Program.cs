365c5bf55472d78aee4c678f1876d650111c0bfc
kneeraj825 <kneeraj825@gmail.com>
2020-04-07 11:38:01 +0530

    Added: Test Case Null check

--- QuantityMeasurementProblem/Program.cs
+++ QuantityMeasurementProblem/Program.cs
@@ -10,9 +10,10 @@ namespace QuantityMeasurementProblem
             //If feet and inch is zero then returns equal
             ReturnEqual returnequal = new ReturnEqual();
             returnequal.feet = 0;
-            returnequal.inch = 0;
-            ReturnEqual returnequal1 = returnequal;
-            Console.WriteLine(returnequal.Equals(returnequal1));
+            ReturnEqual returnequal2 = new ReturnEqual();
+            returnequal2.feet = 0;
+           
+
         }
     }
 }
--- QuantityMeasurementProblem/ReturnEqual.cs
+++ QuantityMeasurementProblem/ReturnEqual.cs
@@ -7,6 +7,22 @@ namespace QuantityMeasurementProblem
     class ReturnEqual
     {
         public int feet {get; set;}
-        public int inch { get; set; }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is ReturnEqual))
+            {
+                return false;
+            }
+            return this.feet == ((ReturnEqual)obj).feet && this.feet == ((ReturnEqual)obj).feet;
+
+        }
+        public override int GetHashCode()
+        {
+            return this.feet.GetHashCode() ^ this.feet.GetHashCode(); 
+        }
     }
 }
