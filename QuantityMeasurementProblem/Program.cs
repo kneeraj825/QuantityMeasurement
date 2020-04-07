ca84563b81eece469bbf3b4110c4a9cba53fa89d
kneeraj825 <kneeraj825@gmail.com>
2020-04-07 11:41:23 +0530

    Added:Test Case Ref check

--- QuantityMeasurementProblem/Program.cs
+++ QuantityMeasurementProblem/Program.cs
@@ -12,8 +12,9 @@ namespace QuantityMeasurementProblem
             returnequal.feet = 0;
             ReturnEqual returnequal2 = new ReturnEqual();
             returnequal2.feet = 0;
-           
-
+            // for the reference equality returns false by different method
+            bool areEqual = System.Object.ReferenceEquals(returnequal, returnequal2);
+            System.Console.WriteLine("ReferenceEquals(value1, value2) = {0}", areEqual);
         }
     }
 }
